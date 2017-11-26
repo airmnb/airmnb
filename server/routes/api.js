8bd80dcd366f5fb88fbea4239a98e6b9d98e7819
Jun Shao <mr.shaojun@gmail.com>
2017-11-26 21:52:07 +1100

    Initial commit

---
+++ server/routes/api.js
@@ -0,0 +1,10 @@
+'user strict';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+exports.router = express.Router();
+/* GET api listing. */
+exports.router.get('/', function (req, res) {
+    res.send('api works');
+});
+//# sourceMappingURL=api.js.map
