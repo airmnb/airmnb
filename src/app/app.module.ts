cdc6784a70d96ce2e9fbc6edcd80c77a26e814eb
cg1101 <gcheng@appen.com>
2018-01-11 07:08:34 +1100

    adjusted import statements in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { registerLocaleData } from '@angular/common';
-// i18n
-import localeEnUs from '@angular/common/locales/en-US-POSIX';
-import localeEnUsExtra from '@angular/common/locales/extra/en-US-POSIX';
-import localeZhHans from '@angular/common/locales/zh-Hans';
-import localeZhHansExtra from '@angular/common/locales/extra/zh-Hans';
-// i18n
-import localeJa from '@angular/common/locales/ja';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarModule } from 'angular-calendar';
-import { Ng2SmartTableModule } from 'ng2-smart-table';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { CoreModule } from './core/core.module';
-
-import { HomeComponent } from './home/home.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { NgbModule, NgbDatepickerModule,  NgbTimepickerModule, NgbTimepickerConfig } from '@ng-bootstrap/ng-bootstrap';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { DateTimePickerComponent } from './date-time-picker/date-time-picker.component';
-import { NotificationComponent } from './notification/notification.component';
-import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
-import { AddressInputComponent } from './address-input/address-input.component';
-import { environment } from '../environments/environment';
-import { SlotListComponent } from './slot-list/slot-list.component';
-import { ImageUploadModule } from "angular2-image-upload";
-import { LoginContentComponent } from './login-content/login-content.component';
-import { SignupContentComponent } from './signup-content/signup-content.component';
-import { SlotComponent } from './slot/slot.component';
-import { ProfileContentComponent } from './profile-content/profile-content.component';
-import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { SlotEditComponent } from './slot-edit/slot-edit.component';
-import { BookingComponent } from './booking/booking.component';
-import { SlotDisplayComponent } from './slot-display/slot-display.component';
-import { BookingListComponent } from './booking-list/booking-list.component';
-import { MapSearchComponent } from './map-search/map-search.component';
-import { MarkerManager } from '@agm/core/services/managers/marker-manager';
+import { registerLocaleData } from '@angular/common';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {  MatMomentDateModule} from "@angular/material-moment-adapter";
+import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider } from "angular4-social-login";
+
 import {
   MatAutocompleteModule,
   MatButtonModule,
@@ -76,32 +42,69 @@ import {
   MatTooltipModule,
   MatStepperModule,
 } from '@angular/material';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {  MatMomentDateModule} from "@angular/material-moment-adapter";
-import { TimepickerComponent } from './timepicker/timepicker.component';
-import { ImageUploaderComponent } from './image-uploader/image-uploader.component';
-import { EventPlaceComponent } from './event-place/event-place.component';
-import { EventPlaceListComponent } from './event-place-list/event-place-list.component';
-import { SelectComponent } from './select/select.component';
-import { DatepickerComponent } from './datepicker/datepicker.component';
+
+// i18n
+// i18n
+import localeEnUs from '@angular/common/locales/en-US-POSIX';
+import localeEnUsExtra from '@angular/common/locales/extra/en-US-POSIX';
+import localeJa from '@angular/common/locales/ja';
+import localeZhHans from '@angular/common/locales/zh-Hans';
+import localeZhHansExtra from '@angular/common/locales/extra/zh-Hans';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+
+import { CalendarModule } from 'angular-calendar';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { NgbModule, NgbDatepickerModule,  NgbTimepickerModule, NgbTimepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { MarkerManager } from '@agm/core/services/managers/marker-manager';
+import { ImageUploadModule } from "angular2-image-upload";
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import { NgxAmapModule } from 'ngx-amap';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+
+import { environment } from '../environments/environment';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { AddressInputComponent } from './address-input/address-input.component';
+import { AddressInputGaodeComponent } from './address-input-gaode/address-input-gaode.component';
+import { AddressInputGoogleComponent } from './address-input-google/address-input-google.component';
 import { AgepickerComponent } from './agepicker/agepicker.component';
 import { BabiesComponent } from './babies/babies.component';
 import { BabyComponent } from './baby/baby.component';
-import { SitesComponent } from './sites/sites.component';
-import { SiteComponent } from './site/site.component';
-import { TakePhotoComponent } from './take-photo/take-photo.component';
-import { TransactionComponent } from './transaction/transaction.component';
 import { BabyAvatarComponent } from './baby-avatar/baby-avatar.component';
 import { BookingCardComponent } from './booking-card/booking-card.component';
-import { LoadingComponent } from './loading/loading.component';
-import {HttpClientModule, HttpClient} from '@angular/common/http';
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import { NgxAmapModule } from 'ngx-amap';
-import { AddressInputGaodeComponent } from './address-input-gaode/address-input-gaode.component';
-import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider } from "angular4-social-login";
+import { BookingListComponent } from './booking-list/booking-list.component';
+import { BookingComponent } from './booking/booking.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import { DateTimePickerComponent } from './date-time-picker/date-time-picker.component';
+import { EventPlaceComponent } from './event-place/event-place.component';
+import { EventPlaceListComponent } from './event-place-list/event-place-list.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ImageUploaderComponent } from './image-uploader/image-uploader.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { AddressInputGoogleComponent } from './address-input-google/address-input-google.component';
+import { LoadingComponent } from './loading/loading.component';
+import { LoginComponent } from './login/login.component';
+import { LoginContentComponent } from './login-content/login-content.component';
+import { MapSearchComponent } from './map-search/map-search.component';
+import { NotificationComponent } from './notification/notification.component';
+import { ProfileContentComponent } from './profile-content/profile-content.component';
+import { SelectComponent } from './select/select.component';
+import { SignupContentComponent } from './signup-content/signup-content.component';
+import { SignupComponent } from './signup/signup.component';
+import { SitesComponent } from './sites/sites.component';
+import { SiteComponent } from './site/site.component';
+import { SlotDisplayComponent } from './slot-display/slot-display.component';
+import { SlotEditComponent } from './slot-edit/slot-edit.component';
+import { SlotListComponent } from './slot-list/slot-list.component';
+import { SlotComponent } from './slot/slot.component';
+import { TakePhotoComponent } from './take-photo/take-photo.component';
+import { TimepickerComponent } from './timepicker/timepicker.component';
+import { TransactionComponent } from './transaction/transaction.component';
 
 const sso_config = new AuthServiceConfig([
   {
