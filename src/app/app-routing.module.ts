7c619ae000ba4713f4b57100c0f27e145f76431a
cg1101 <gcheng@appen.com>
2018-01-11 22:44:01 +1100

    removed importing of CommonModule from app-routing.module for it is not used

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import {NgModule}                from '@angular/core';
-import {CommonModule}            from '@angular/common';
 import {RouterModule, Routes}    from '@angular/router';
 
 import {AboutusComponent}        from './aboutus/aboutus.component';
@@ -52,14 +51,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
