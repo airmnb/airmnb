518a5c5e7b5ecab65faad4204d2917c209c2c5b1
Jun Shao <mr.shaojun@gmail.com>
2018-01-23 23:33:54 +1100

    Facebook SSO

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -3,6 +3,8 @@ import * as express from 'express';
 import * as passport from "passport";
 import * as passport_google from 'passport-google-oauth20';
 const GoogleStrategy = passport_google.Strategy;
+import * as passport_facebook from 'passport-facebook';
+const FacebookStrategy = passport_facebook.Strategy;
 import env from '../env';
 import { dataGatewayFactory } from "../data/gateway";
 import { Account } from '../../types';
@@ -14,6 +16,9 @@ export const router = express.Router();
 const host = process.env.URL_HOST || "localhost";
 const accountApi = dataGatewayFactory.produce('account');
 
+/**
+ * Google SSO settings
+ */
 passport.use(new GoogleStrategy({
   clientID: env.googleClientId,
   clientSecret: env.googleClientSecret,
@@ -22,12 +27,36 @@ passport.use(new GoogleStrategy({
 async (accessToken, refreshToken, profile, cb) => {
   // console.log('Google SSO profile', JSON.stringify(profile));
   const email = profile.emails[0].value;
-  let account : Account = await accountApi.queryOne({name: email});
+  const name = email + '@@google';
+  let account : Account = await accountApi.queryOne({name: name});
   if(!account) {
     account = {
       id: uuid.v4(),
       email: email,
-      name: email,
+      name: name,
+      secret: null,
+      enabled: true
+    };
+    await accountApi.create(account);
+  }
+  cb(null, account);
+}
+));
+
+passport.use(new FacebookStrategy({
+  clientID: env.facebookAppId,
+  clientSecret: env.facebookAppSecret,
+  callbackURL: `https://${host}/auth/facebook/callback`
+},
+async (accessToken, refreshToken, profile, cb) => {
+  console.log('Facebook SSO profile', JSON.stringify(profile));
+  const name = profile.id + '@@facebook';
+  let account : Account = await accountApi.queryOne({name: name});
+  if(!account) {
+    account = {
+      id: uuid.v4(),
+      email: null,
+      name: name,
       secret: null,
       enabled: true
     };
@@ -61,3 +90,19 @@ router.get('/google/callback',
     res.cookie('c', JSON.stringify(c));
     res.redirect('/');
   });
+
+// Facebook SSO
+router.get('/facebook', passport.authenticate('facebook', { scope: ['public_profile', 'email'] }));
+router.get('/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  (req, res) => {
+    const account = req['user'];
+    // console.log('Google SSO request', JSON.stringify(req['user']));
+    // Successful authentication, redirect home.
+    const c = {
+      account,
+      role: 0
+    };
+    res.cookie('c', JSON.stringify(c));
+    res.redirect('/');
+});
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,5 +11,7 @@ export const environment = {
   googleApiKey: 'AIzaSyAmXvvrwCbeg-GNQxsFVhYOICg0ta8Bjps',
   googleClientId: '13711557288-5j8je109l29crtnrg80mudkh88sikmho.apps.googleusercontent.com',
   googleClientSecret: 'wASTuBK1N0pHsmnSDG3_MNVs',
+  facebookAppId: '202919500264403',
+  facebookAppSecret: 'a20ea1b64d72e6291a661a8757060024',
   gaodeApiKey: '771eef4f59814509ac4ffb78f480d544'
 };
