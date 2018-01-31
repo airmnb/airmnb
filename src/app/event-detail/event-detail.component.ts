438c7825f2fee5fdb54f13a155d28b6f3c39924a
Jun Shao <mr.shaojun@gmail.com>
2018-01-31 23:28:56 +1100

    Not slide carousel images

--- src/app/event-detail/event-detail.component.ts
+++ src/app/event-detail/event-detail.component.ts
@@ -17,7 +17,6 @@ export class EventDetailComponent implements OnInit {
     grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
     slide: 1,
     speed: 400,
-    interval: 4000,
     point: {
       visible: true,
       pointStyles: `
