9cb690f64fa3e698636c4e4f9ec0c57b35d8e97b
Jun Shao <mr.shaojun@gmail.com>
2018-04-05 22:56:19 +1000

    Add ssl certificate for www.airmombaby.com

--- configs/config.airmombaby.ts
+++ configs/config.airmombaby.ts
@@ -1,6 +1,6 @@
 export const config = {
   host_url: 'www.airmombaby.com',
-  key_file: 'ssl_certificates/www.airmombaby.com/some.key',
-  crt_file: 'ssl_certificates/www.airmombaby.com/some.crt',
-  ca_files: []
+  key_file: 'ssl_certificates/www.airmombaby.com/Apache/www.airmombaby.com.key',
+  crt_file: 'ssl_certificates/www.airmombaby.com/Apache/www.airmombaby.com.crt',
+  ca_files: ['ssl_certificates/www.airmombaby.com/Apache/gd_bundle-www.airmombaby.com.crt']
 };
